f7ce494d20c970030cae808eb9f5e9f047e4bed1
Raf <rt3426@protonmail.com>
2023-08-08 13:57:42 -0400

    import path attempt

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,7 +1,7 @@
 import pytest
-import sys
+import os, sys
 import requests_mock
-sys.path.append('backend')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
 from app import create_app
 
 @pytest.fixture
