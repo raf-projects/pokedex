5e3927e50ebf10b9fa0cca8602bd2068e79ef683
Raf <rt3426@protonmail.com>
2023-08-09 09:40:14 -0400

    fixing failing deploy 2(why are these failing now?? - was it py run.py &?)

--- .platform/hooks/postdeploy/03_flask_start.sh
+++ .platform/hooks/postdeploy/03_flask_start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-echo "Will this run?" >> /var/log/out.log
-echo "This actually ran $(date)" >> /var/log/out.log
+echo "Script Run" >> /var/log/out.log
+echo "RUNTIME: $(date)" >> /var/log/out.log
 cd /var/app/current/backend
 echo "PWD: $(pwd)" >> /var/log/out.log
 ls -l >> /var/log/out.log
+echo "" >>  /var/log/out.log
 export PYTHONPATH=$PYTHONPATH:/var/app/current/backend/dependencies
+echo "" >>  /var/log/out.log
 echo "PYPATH: $(echo $PYTHONPATH)" >> /var/log/out.log
 python3.11 run.py & >> /var/log/out.log 2>&1
