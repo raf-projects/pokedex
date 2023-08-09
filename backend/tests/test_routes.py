6b6670842a694ad44724dc638751cf3cac94a3ff
Raf <rt3426@protonmail.com>
2023-08-09 07:54:35 -0400

    rm ebextensions

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,9 +1,9 @@
 import pytest
 import os, sys
 import requests_mock
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'app'))
 # sys.path.append('backend')
-from app import create_app
+from main import create_app
 
 @pytest.fixture
 def client():
